1157c18bdd7b6f242152bb7f5e3863b7db9569c0
Torxanic <76171820+Torxanic@users.noreply.github.com>
2021-03-27 21:20:10 +0100

    REFACTOR: "Avatar Konfigurieren" Bug behoben wegen Imports

--- src/main/java/com/de/dhbw/hb/mud/service/registration/AuthService.java
+++ src/main/java/com/de/dhbw/hb/mud/service/registration/AuthService.java
@@ -4,7 +4,6 @@ import com.de.dhbw.hb.mud.model.UserDto;
 import com.de.dhbw.hb.mud.repository.UserRepository;
 import com.de.dhbw.hb.mud.service.registration.exception.AuthException;
 import com.de.dhbw.hb.mud.service.registration.exception.RegisterException;
-import com.de.dhbw.hb.mud.views.AvatarErstellen.AvatarErstellenView;
 import com.de.dhbw.hb.mud.views.AvatarKonfigurator.AvatarErstellenView;
 import com.de.dhbw.hb.mud.views.Konfigurator.AvatarKonfiguratorView;
 import com.de.dhbw.hb.mud.views.TestView;
