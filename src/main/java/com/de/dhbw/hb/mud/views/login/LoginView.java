b343cefa31a34c8fb885c47416d467d25977c532
Lukas2877 <67147431+Lukas2877@users.noreply.github.com>
2021-03-29 00:15:42 +0200

    REFACTOR: Überschrift der Login-Page geändert und Text hinzugefügt.

--- src/main/java/com/de/dhbw/hb/mud/views/login/LoginView.java
+++ src/main/java/com/de/dhbw/hb/mud/views/login/LoginView.java
@@ -6,6 +6,7 @@ import com.de.dhbw.hb.mud.service.registration.exception.AuthException;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -44,20 +45,21 @@ public class LoginView extends VerticalLayout {
             try {
                 if (VaadinSession.getCurrent().getAttribute(UserDto.class) != null
                 && VaadinSession.getCurrent().getAttribute(UserDto.class).getName().equals(name.getValue())){
-                    Notification.show("Sie sind bereits Angemeldet!");
+                    Notification.show("Sie sind bereits angemeldet!");
                 }else {
                     authService.authenticate(name.getValue(),passwordField.getValue());
                 }
                 UI.getCurrent().navigate("about");
 
             } catch (AuthException authException) {
-                Notification.show("fehler bei der Anmeldung. Prüfen Sie ihre Daten!");
+                Notification.show("Fehler bei der Anmeldung. Prüfen Sie ihre Daten!");
             }
 
         });
 
         add(
-                new H1("Binäratops dungeon login"),
+                new H1("Binäratest - Login"),
+                new H2("Willkommen zu unserem Prototypen!\nBitte erstellen Sie sich einen Account, falls Sie noch keinen haben. ;)"),
                 name,
                 passwordField,
                 logInButton,
