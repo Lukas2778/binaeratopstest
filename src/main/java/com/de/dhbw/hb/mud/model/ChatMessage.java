5d13727c9ab1e22a8ad1978193cd6e4395b65faf
Preuer <i19051@hb.dhbw-stuttgart.de>
2021-03-28 14:40:44 +0200

    FEATURE: BUGFIX

---
+++ src/main/java/com/de/dhbw/hb/mud/model/ChatMessage.java
@@ -0,0 +1,21 @@
+package com.de.dhbw.hb.mud.model;
+
+import java.time.LocalDateTime;
+
+public class ChatMessage {
+    private String from;
+    private LocalDateTime time;
+    private String message;
+
+    public ChatMessage(String from, String message){
+        this.from = from;
+        this.message = message;
+        this.time = LocalDateTime.now();
+    }
+    public String getFrom() {return from; }
+
+    public LocalDateTime getTime() {return time; }
+
+
+    public String getMessage(){return message;}
+}
