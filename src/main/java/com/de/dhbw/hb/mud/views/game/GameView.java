5663704c4caeafa04968c627bdc23f35245a1cc9
Preuer <i19051@hb.dhbw-stuttgart.de>
2021-03-31 18:31:39 +0200

    FEATURE: Game-Logik

--- src/main/java/com/de/dhbw/hb/mud/views/game/GameView.java
+++ src/main/java/com/de/dhbw/hb/mud/views/game/GameView.java
@@ -8,6 +8,8 @@ import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -17,12 +19,16 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.server.VaadinSession;
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.UnicastProcessor;
 
 import java.util.Random;
 
+@CssImport("./views/helloworld/game-view.css")
 @PageTitle("Game")
 public class GameView extends VerticalLayout {
 
@@ -33,8 +39,11 @@ public class GameView extends VerticalLayout {
 
 
 
+
     public GameView(UnicastProcessor<ChatMessage> publisher,
-                    Flux<ChatMessage> messages) {
+                    Flux<ChatMessage> messages, DungeonDataService dungeonDataService) {
+
+        this.dungeonDataService = dungeonDataService;
 
 
 
@@ -42,7 +51,9 @@ public class GameView extends VerticalLayout {
 
         //TODO Später mit dem Parameter Dungeonmaster ändern
 
-        VerticalLayout controllsView = new VerticalLayout();
+        VerticalLayout controllsView =new VerticalLayout();
+        HorizontalLayout ButtonView =new HorizontalLayout();
+
         if(true){
             chat.setUsername("Dungeon Master");
             TextField role=new TextField("Rolle");
@@ -62,6 +73,45 @@ public class GameView extends VerticalLayout {
 
         } else{
             chat.setUsername(VaadinSession.getCurrent().getAttribute(UserDto.class).getName());
+            TextField role=new TextField("Rolle");
+            role.setValue(chat.getUsername());
+            role.setReadOnly(true);
+            role.addThemeVariants();
+
+
+
+            controllsView.add(role );
+
+
+
+
+            StringBuilder sB = new StringBuilder();
+            dungeonDataService.findItemsInRoom(12).stream().map(item -> item.getName()).forEach(sB::append);
+
+            //Problem -> Keine Absätze Möglich
+            String result = dungeonDataService.findItemsInRoom(12).stream().map(item -> item.getName()).collect(Collectors.joining("\n"));
+
+
+//            private void itemsAusgeben() {
+//                for ( int i = 0; i < dungeonDataService.findItemsInRoom(12)
+//                        .size(); i++ )
+//                {
+//
+//                    publisher.onNext(new ChatMessage("Items: ",
+//                                                     dungeonDataService.findItemsInRoom(12)
+//                                                             .get(i)
+//                                                             .getName()));
+//                }
+//            }
+
+
+
+            Button Umschauen = new Button("Umschauen", e -> publisher.onNext(new ChatMessage("Items: ", result)));
+            Button Aufheben = new Button("Aufheben", e -> publisher.onNext(new ChatMessage("Items: ", dungeonDataService.findItemsInRoom(12).get(0).getName())));
+            Button Ansprechen = new Button("Aufheben", e -> publisher.onNext(new ChatMessage("Items: ", dungeonDataService.findItemsInRoom(12).get(0).getName())));
+            Button Bewegen = new Button("Aufheben", e -> publisher.onNext(new ChatMessage("Items: ", dungeonDataService.findItemsInRoom(12).get(0).getName())));
+            ButtonView.add( Umschauen, Aufheben );
+
         }
 
 
@@ -71,13 +121,13 @@ public class GameView extends VerticalLayout {
 
 
         Label secondLabel = new Label("Platzhalter für eine Karte des Dungeons");
-        //label thirdLabel = new Label("Rolle:                                           ");
+        //Label thirdLabel = new Label("Rolle: " + chat.getUsername());
 
 
         SplitLayout innerLayout = new SplitLayout();
         innerLayout.setOrientation(SplitLayout.Orientation.VERTICAL);
         innerLayout.addToPrimary(secondLabel);
-        innerLayout.addToSecondary(controllsView);
+        innerLayout.addToSecondary(controllsView, ButtonView);
         innerLayout.setSizeFull();
 
         SplitLayout layout = new SplitLayout();
@@ -89,4 +139,5 @@ public class GameView extends VerticalLayout {
         add(new HorizontalLayout(new Text("Dungeonname"), new Button("Spiel verlassen",
                 e-> UI.getCurrent().navigate("about"))),layout);
     }
+
 }
